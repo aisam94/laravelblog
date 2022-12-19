771c6e814fdc41a9268b6f186432883f4fb10740
Aisamuddin <aisam0094@gmail.com>
2022-12-19 18:13:07 +0800

    add categories

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    // return category of post
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
