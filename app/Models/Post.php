aa911723eaa37b71e3fb85bf93f7fc7ae685ca67
Aisamuddin <aisam0094@gmail.com>
2022-12-29 22:25:45 +0800

    add user id to posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -16,4 +16,10 @@ class Post extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    // Post is written by a user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
