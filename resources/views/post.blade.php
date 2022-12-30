02f028a628416d01a3c1d4e6460b3c62f49ade90
Aisamuddin <aisam0094@gmail.com>
2022-12-30 17:31:53 +0800

    added layout blade and etc

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,20 +1,19 @@
-<!DOCTYPE html>
-<title>My Blog</title>
-<link rel="stylesheet" href="/app.css">
-<!-- <script src="/app.js"></script> -->
+@extends('components/layout')
+@section('content')
 
-<body>
-    <article>
-        <h1>
-            <?= $post->title; ?>
-        </h1>
-        <p>
-            By <a href='/authors/<?= $post->author->username ?>'><?= $post->user->name ?></a> in
-            <a href="/categories/<?= $post->author->name ?>"><?= $post->category->name ?></a>
-        </p>
-        <div>
-            <?= $post->body; ?>
-        </div>
-    </article>
-    <a href="/">Go Back</a>
-</body>
+<article>
+    <h1>
+        <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+    </h1>
+    <p>
+        By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in
+        <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+    </p>
+    <div>
+        {{ $post->body }}
+    </div>
+</article>
+
+<a href="/">Go Back</a>
+
+@endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,38 +1,22 @@
-<!DOCTYPE html>
-<title>My Blog</title>
-<link rel="stylesheet" href="/app.css">
-<!-- <script src="/app.js"></script> -->
+@extends('components/layout')
+@section('content')
 
-<body>
-    <?php foreach ($posts as  $post) : ?>
-        <article>
-            <h1>
-                <a href="/posts/<?= $post->slug; ?>">
-                    <?= $post->title; ?>
-                </a>
-            </h1>
-            <p>
-                By <a href='/authors/<?= $post->author->username ?>'><?= $post->author->name ?></a> in
-                </a><a href="/categories/<?= $post->category->slug ?>"><?= $post->category->name ?></a>
-            </p>
-            <div>
-                <?= $post->excerpt; ?>
-            </div>
-        </article>
-    <?php endforeach; ?>
-    <!-- <article>
-        <h1><a href="/post/my-first-post">My First Post</a></h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In minima, qui, recusandae neque officiis dolorum impedit nostrum illum numquam cum sed ullam ea? Labore, ea officiis enim sed animi ipsum!</p>
-    </article>
+@foreach ($posts as $post)
+<article>
+    <h1>
+        <a href="/posts/{{ $post->slug }}">
+            {{ $post->title }}
+        </a>
+    </h1>
+    <p>
+        By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in
+        </a><a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+    </p>
+    <div>
+        {{ $post->excerpt }}
+    </div>
+</article>
 
-    <article>
-        <h1><a href="/post/my-second-post">My Second Post</a></h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In minima, qui, recusandae neque officiis dolorum impedit nostrum illum numquam cum sed ullam ea? Labore, ea officiis enim sed animi ipsum!</p>
-    </article>
+@endforeach
 
-    <article>
-        <h1><a href="/post/my-third-post">My Third Post</a></h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In minima, qui, recusandae neque officiis dolorum impedit nostrum illum numquam cum sed ullam ea? Labore, ea officiis enim sed animi ipsum!</p>
-    </article> -->
-
-</body>
+@endsection
