8eca9490301769b4b1b1a082987fbf00874c1cf9
Aisamuddin <aisam0094@gmail.com>
2022-12-31 20:33:44 +0800

    making the pages dynamic

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,18 +4,21 @@
 @include('components/_posts-header')
 
 <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-    @include('components/post-featured-card')
+    @if ($posts->count())
+    @include('components/post-featured-card', ['post' => $posts[0]])
 
     <div class="lg:grid lg:grid-cols-2">
-        @include('components/post-card')
-        @include('components/post-card')
+        @foreach( $posts->skip(1) as $post)
+        @include('components/post-card', ['post' => $post])
+        @endforeach
     </div>
     <div class="lg:grid lg:grid-cols-3">
-        @include('components/post-card')
-        @include('components/post-card')
-        @include('components/post-card')
-        @include('components/post-card')
+        <!-- @include('components/post-card') -->
     </div>
+
+    @else
+    <p class='text-center'>No posts yet. Please check back later.</p>
+    @endif
 </main>
 
 @endsection
