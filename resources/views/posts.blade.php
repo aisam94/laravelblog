37987f43e62da2571b8d7c33f25ea727fa5431bd
Aisamuddin <aisam0094@gmail.com>
2022-12-31 19:38:40 +0800

    change html and css into blade template

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,22 +1,21 @@
 @extends('components/layout')
 @section('content')
 
-@foreach ($posts as $post)
-<article>
-    <h1>
-        <a href="/posts/{{ $post->slug }}">
-            {{ $post->title }}
-        </a>
-    </h1>
-    <p>
-        By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in
-        </a><a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-    </p>
-    <div>
-        {{ $post->excerpt }}
-    </div>
-</article>
+@include('components/_posts-header')
 
-@endforeach
+<main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+    @include('components/post-featured-card')
+
+    <div class="lg:grid lg:grid-cols-2">
+        @include('components/post-card')
+        @include('components/post-card')
+    </div>
+    <div class="lg:grid lg:grid-cols-3">
+        @include('components/post-card')
+        @include('components/post-card')
+        @include('components/post-card')
+        @include('components/post-card')
+    </div>
+</main>
 
 @endsection
