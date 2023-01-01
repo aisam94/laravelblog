45924747573f5908c67f4858c10385ed191c5483
Aisamuddin <aisam0094@gmail.com>
2023-01-01 20:49:41 +0800

    add login

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,12 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\ValidationException;
 use Illuminate\Http\Request;
 
 class SessionController extends Controller
 {
-    public function destroy(){
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function destroy()
+    {
         auth()->logout();
         return redirect('/')->with('success', 'GoodBye!!!');
+    }
+
+    public function store(){
+        // validate request
+        $attribute = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        // authenticate and login user based on credentials provided
+        if (auth()->attempt($attribute)){
+            // redirect
+            return redirect('/')->with('success', 'Welcome back!');
+        }
+
+        throw ValidationException::withMessages([
+            'email' => 'Credentials cannot be verified'
+        ]);
     }
 }
